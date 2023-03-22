0c962a621a3675562426126c2aac64107979c10a
matt marques <70326551+mestre-dos-magos@users.noreply.github.com>
2023-03-22 01:29:52 +0000

    api changes - melhorias

--- API.py
+++ API.py
@@ -3,33 +3,36 @@ import requests
 import json
 from pprint import pprint
 
-KEYAPI = ''
+KEYAPI = 'acccd88416ddb8b9bc489dfdb1ec2972'
 
-CITY = ''
+CITY = 'Manaus'
 
 class API:
     def __init__(self, API = KEYAPI, city=CITY):
         self.__API_key = API
         self.__city = city
+        self.request_data()
+
+    def request_data(self):
         self.__base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" + self.__API_key + "&q=" + self.city
-        
-        
         self.weather_data = requests.get(self.__base_url).json()
         if weather_data['cod'] != '404':
             #temperaturas:
-            self.var_kelvin = self.weather_data['main']['temp']
-            self.var_celcius = self.var_kelvin - 273.15
-            self.var_fahrenheit = self.self.var_celcius * (9/5) + 32
+            self.__var_kelvin = self__.weather_data['main']['temp']
+            self.__var_celcius = self__.var_kelvin - 273.15
+            self.__var_fahrenheit = self__.self.var_celcius * (9/5) + 32
             #pressão:
-            self.var_pressure = self.weather_data['main']['pressure']
+            self.__var_pressure = self__.weather_data['main']['pressure']
             #humidade:
-            self.var_humidity = self.weather_data['main']['humidity']
+            self.__var_humidity = self__.weather_data['main']['humidity']
             #descrição:
-            val_weather = self.weather_data['main']['weather']
-            self.description = val_weather[0]['description']
+            val_weather = self.__weather_data['main']['weather']
+            self.__description = val_weather[0]['description']
+        else:
+            print(" City Not Found ")
 
     def change_city(self, new_city):
-        self.__city = new_city
-
+        self.__city = new_cityelse:
+        self.request_data()
 
 API = API()
