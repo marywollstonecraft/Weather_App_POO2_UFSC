f2e3172c7796b1b98462a0c682f9decad43f167a
matt marques <70326551+mestre-dos-magos@users.noreply.github.com>
2023-03-23 03:38:46 +0000

    primeira versão da API com extração funcionando

--- API.py
+++ API.py
@@ -1,24 +1,34 @@
+import datetime as dt
 import requests
+import json
 
-CITY = 'Manaus'
-APIkey = 'acccd88416ddb8b9bc489dfdb1ec2972'
+def get_data(CITY= 'London'):
+    BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+    API_KEY = 'acccd88416ddb8b9bc489dfdb1ec2972'
+    url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+    data_json = requests.get(url).json()
+    return data_json
+
+def kelvin_to_celcius_fahrenheit(kelvin):
+    celcius = kelvin - 273.15
+    fahrenheit = celcius * (9/5) + 32
+    return celcius, fahrenheit
 
 class API:
-    def __init__(self, api, city):
-        self.API_key = api
-        self.city = city
-        self.base_url = "http://api.openweathermap.org/data/2.5/weather?appid=" + self.API_key + "&q=" + self.city
-        self.weather_data = requests.get(self.base_url).json()
+    def __init__(self):
+        self.weather_data = get_data()
+
+        with open("weather_data.json", "w") as outfile:
+            json.dump(self.weather_data, outfile)
+
         if self.weather_data['cod'] == 200:
             #temperaturas medias:
             self.var_kelvin = self.weather_data['main']['temp']
-            self.var_celcius = self.var_kelvin - 273.15
-            self.var_fahrenheit = self.var_celcius * (9/5) + 32
+            self.var_celcius, self.var_fahrenheit = kelvin_to_celcius_fahrenheit(self.var_kelvin)
 
             # sensacao de temperatura
             self.var_feels_like_kelvin = self.weather_data['main']['feels_like']
-            self.var_feels_like_celcius = self.var_feels_like_kelvin - 273.15
-            self.var_feels_like_fahrenheit = self.var_feels_like_celcius * (9/5) + 32
+            self.var_feels_like_celcius, self.var_feels_like_fahrenheit = kelvin_to_celcius_fahrenheit(self.var_feels_like_kelvin)
             
             #pressão:
             self.var_pressure = self.weather_data['main']['pressure']
@@ -42,11 +52,12 @@ class API:
             # nuvens
             self.var_clouds = self.weather_data['clouds']
 
+            #horarios 
+            self.sunrise_time = dt.datetime.utcfromtimestamp(self.weather_data['sys']['sunrise'] + self.weather_data['timezone'])
+            self.sunset_time = dt.datetime.utcfromtimestamp(self.weather_data['sys']['sunset'] + self.weather_data['timezone'])
+
 
         else:
             print(" City Not Found ")
 
-    def change_city(self, new_city):
-        self.city = new_city
-
-API = API(APIkey, CITY)
+API = API()
