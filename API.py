c9c9de661ee15d48bcffa90eb0dbc79b40011a4b
matt marques <70326551+mestre-dos-magos@users.noreply.github.com>
2023-04-08 15:39:52 +0000

    API changes - removing some variables & better code with ARGs to Class

--- API.py
+++ API.py
@@ -2,23 +2,20 @@ import datetime as dt
 import requests
 import json
 
-def get_data(CITY= 'London'):
-    BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
-    API_KEY = 'acccd88416ddb8b9bc489dfdb1ec2972'
-    url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
-    data_json = requests.get(url).json()
-    return data_json
-
 def kelvin_to_celcius_fahrenheit(kelvin):
     celcius = kelvin - 273.15
     fahrenheit = celcius * (9/5) + 32
     return celcius, fahrenheit
 
-class API:
-    def __init__(self):
-        self.weather_data = get_data()
+class Api:
+    def __init__(self, CITY= 'manaus', METRICS= 'metric'):
+        #extraindo dados da API
+        self.BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+        self.API_KEY = 'acccd88416ddb8b9bc489dfdb1ec2972'
+        url = self.BASE_URL + "appid=" + self.API_KEY + "&q=" + CITY + "&metrics=" + METRICS
+        self.weather_data = requests.get(url, timeout=10).json()
 
-        with open("weather_data.json", "w") as outfile:
+        with open("data/weather_data.json", "w", encoding='UTF-8') as outfile:
             json.dump(self.weather_data, outfile)
 
         if self.weather_data['cod'] == 200:
@@ -37,17 +34,10 @@ class API:
             self.var_humidity = self.weather_data['main']['humidity']
             
             #descrição:
-            self.var_condition = self.weather_data['weather']['main']
             self.description = self.weather_data['weather'][0]['description']
             
             #vento:
             self.var_wind = self.weather_data['wind']['speed']
-            
-            # nivel do mar
-            self.var_sea_level = self.weather_data['main']['sea_level']
-            
-            # altitute terrestre
-            self.var_grnd_level = self.weather_data['main']['grnd_level']
             
             # nuvens
             self.var_clouds = self.weather_data['clouds']
@@ -60,4 +50,4 @@ class API:
         else:
             print(" City Not Found ")
 
-API = API()
+API = Api()
